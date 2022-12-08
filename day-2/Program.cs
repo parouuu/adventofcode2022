c5d803b2bfb0f2cade20c3e9f1b62525267c31e6
Stéphane <sverez@activeassurances.fr>
2022-12-08 18:37:04 +0100

    wip day 2

---
+++ day-2/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    static Dictionary<string, RpsEnum> mapping = new Dictionary<string, RpsEnum>()
+        {
+            { "A", RpsEnum.Rock },
+            { "B", RpsEnum.Paper },
+            { "C", RpsEnum.Scissor },
+            { "X", RpsEnum.Rock },
+            { "Y", RpsEnum.Paper },
+            { "Z", RpsEnum.Scissor }
+        };
+
+    private static void Main(string[] args)
+    {
+        IEnumerable<string> input = File.ReadLines("""C:\Git\adventofcode2022\day-2\input.txt""");
+
+
+        foreach (string line in input)
+        {
+            var splittedLine = line.Split(' ');
+
+            var elf = mapping[splittedLine.First()];
+            var hero = mapping[splittedLine.Last()];
+        }
+
+    }
+}
+
+enum RpsEnum
+{
+    Rock,
+    Paper,
+    Scissor
+};
