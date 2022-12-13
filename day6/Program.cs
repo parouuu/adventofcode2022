6e9f6574ef9dd159f1b80e39e3b105bf3d9a61b0
Stéphane <sverez@activeassurances.fr>
2022-12-13 14:53:34 +0100

    day six answers

---
+++ day6/Program.cs
@@ -0,0 +1,24 @@
+﻿
+/*
+ *    mjqjpqmgbljsphdztnvjfqwrcgsmlb
+ */
+
+string input = File.ReadLines("""C:\Git\adventofcode2022\day6\input.txt""").ToList().First();
+
+bool found = true;
+for (int i = 0; i + 14 < input.Count(); i++)
+{
+    var str = input.Substring(i, 14);
+    found = true;
+    foreach (char c in str)
+    {
+        if (str.Count(s => s == c) >= 2)
+        {
+            found = false;
+        }
+    }
+    if (found)
+    {
+        Console.WriteLine(i + 14);
+    }
+}
