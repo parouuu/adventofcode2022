836158f1129fe07fb5e0c5145a17b725eaf3e535
StéphaneV <4005967+parouuu@users.noreply.github.com>
2022-12-14 18:36:54 +0100

    day seven sloppy code that passes

---
+++ day7/DeviceDirectory.cs
@@ -0,0 +1,32 @@
+﻿public class DeviceFile
+{
+    public string Name { get; set; }
+    public int Size { get; set; }
+}
+
+public class DeviceDirectory
+{
+    public string Name { get; set; }
+
+    public List<DeviceDirectory> DirList = new List<DeviceDirectory>();
+    public List<DeviceFile> FileList = new List<DeviceFile>();
+    public DeviceDirectory Parent { get; set; }
+
+
+    public void AddDirectory(DeviceDirectory element)
+    {
+        DirList.Add(element);
+    }
+
+    public void AddFile(DeviceFile element)
+    {
+        FileList.Add(element);
+    }
+
+    public int GetSize()
+    {
+        var fileSize = FileList.Where(x => x.GetType() == typeof(DeviceFile)).Sum(x => x.Size);
+        var dirSize = DirList.Where(x => x.GetType() == typeof(DeviceDirectory)).Sum(x => x.GetSize());
+        return fileSize + dirSize;
+    }
+}
--- day7/Program.cs
+++ day7/Program.cs
@@ -1,5 +1,89 @@
-﻿
+﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
-var input2 = File.ReadLines($"{Environment.CurrentDirectory}/input.txt").ToList();
+var input = File.ReadLines($"{Environment.CurrentDirectory}/input.txt").ToList();
 
-var test = "";
+Regex cdRegex = new Regex(@"\$\s+(cd)\s+(.+)", RegexOptions.IgnoreCase);
+Regex dirRegex = new Regex(@"(dir)\s+(\w+)", RegexOptions.IgnoreCase);
+Regex lsRegex = new Regex(@"\$\s?ls\s?", RegexOptions.IgnoreCase);
+Regex fileRegex = new Regex(@"(\d+)\s+(.+)", RegexOptions.IgnoreCase);
+
+var home = new DeviceDirectory()
+{
+    Name = "/"
+};
+
+// Console.WriteLine(match.Groups[0]);
+
+DeviceDirectory current = home;
+
+foreach (var line in input)
+{
+    var match = cdRegex.Match(line);
+    if (match.Success)
+    {
+        var value = match.Groups[2].Value;
+
+        if (value == "..")
+        {
+            current = current.Parent;
+        }
+        else
+        {
+            var dir = current.DirList.First(e => e.Name == value);
+            current = dir;
+        }
+    }
+
+    match = dirRegex.Match(line);
+    if (match.Success)
+    {
+        var value = match.Groups[2].Value;
+
+        DeviceDirectory dir = new DeviceDirectory
+        {
+            Name = value,
+            Parent = current,
+        };
+        current.DirList.Add(dir);
+    }
+
+    match = lsRegex.Match(line);
+    if (match.Success)
+    {
+    }
+
+    match = fileRegex.Match(line);
+    if (match.Success)
+    {
+        var value1 = match.Groups[1].Value;
+        var value2 = match.Groups[2].Value;
+
+        DeviceFile file = new DeviceFile
+        {
+            Name = value2,
+            Size = Convert.ToInt32(value1),
+        };
+        current.FileList.Add(file);
+    }
+}
+
+var dirOkList = new List<DeviceDirectory>();
+
+int ret = GoThroughDirs(home);
+int ret2 = 0;
+
+
+static int GoThroughDirs(DeviceDirectory directory)
+{
+    int val = 0;
+
+    foreach (var dir in directory.DirList)
+    {
+        val += GoThroughDirs(dir);
+    }
+
+    if (directory.GetSize() < 100000)
+        val += directory.GetSize();
+    return val;
+}
