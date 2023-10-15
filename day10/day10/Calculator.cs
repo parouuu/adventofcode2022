8cfe6a645db69515e98e04618998ebdbebd579d5
Stéphane V/ <sverez@activeassurances.fr>
2023-10-15 11:54:54 +0200

    day10 exercice 1

---
+++ day10/day10/Calculator.cs
@@ -0,0 +1,44 @@
+﻿internal class Calculator
+{
+    public int CycleNumber { get; set; }
+    public int TotalValue { get; set; }
+    public int Result { get; set; }
+
+    List<int> CountedCycles = new List<int> { 20, 60, 100, 140, 180, 220 , 10000000 };
+    public int NextCycle { get; set; }
+
+    public Calculator()
+    {
+        TotalValue = 1;
+        Result = 0;
+        NextCycle = CountedCycles.First();
+    }
+
+    internal void Add(string line)
+    {
+        if (line == "noop")
+        {
+            CycleNumber++;
+        }
+        else
+        {
+            CycleNumber += 2;
+        }
+
+        if (NextCycle <= CycleNumber)
+        {
+            Result += TotalValue * NextCycle;
+
+            NextCycle = CountedCycles.Where(x => x > NextCycle).FirstOrDefault();
+        }
+
+        if (line != "noop")
+        {
+            var number = default(int);
+            int.TryParse(line.Split(' ')[1], out number);
+            TotalValue += number;
+        }
+    }
+
+    internal int GetResult() => Result;
+}
--- day10/day10/Program.cs
+++ day10/day10/Program.cs
@@ -1,1 +1,10 @@
 ﻿var input = File.ReadLines($"{Environment.CurrentDirectory}/input.txt").ToList();
+
+Calculator calculator = new Calculator();
+
+foreach (var line in input)
+{
+    calculator.Add(line);
+}
+
+Console.WriteLine($"Result is : {calculator.GetResult()}");
