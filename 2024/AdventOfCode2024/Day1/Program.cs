8e686af48765b3191aa5681ba03a14bb49675f46
Stéphane Verez <sverez@assurea.fr>
2024-12-06 09:40:53 +0100

    2024 day one init

---
+++ 2024/AdventOfCode2024/Day1/Program.cs
@@ -0,0 +1,23 @@
+﻿List<string> input = File.ReadLines($"{Environment.CurrentDirectory}/input.txt").ToList();
+
+
+List<int> listOne = new();
+List<int> listTwo = new();
+
+foreach (string str in input)
+{
+    var strSplit = str.Split("   ");
+    listOne.Add(int.Parse(strSplit[0]));
+    listTwo.Add(int.Parse(strSplit[1]));
+}
+
+listOne.Sort();
+listTwo.Sort();
+
+int sum = 0;
+for (int i = 0; i < listOne.Count; i++)
+{
+    sum += Math.Abs(listOne[i] - listTwo[i]);
+}
+
+Console.WriteLine(sum);
