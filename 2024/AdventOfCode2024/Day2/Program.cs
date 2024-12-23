b9b600d1c4db245e6b80c84f6119e4148a46122d
Stéphane V/ <sverez@activeassurances.fr>
2024-12-23 22:00:06 +0100

    day 2 part 1

--- 2024/AdventOfCode2024/Day2/Program.cs
+++ 2024/AdventOfCode2024/Day2/Program.cs
@@ -1,1 +1,26 @@
 ﻿List<string> input = File.ReadLines($"{Environment.CurrentDirectory}/input.txt").ToList();
+
+int safeReportCount = 0;
+
+foreach (string line in input)
+{
+    int[] levels = line.Split(' ').Select(int.Parse).ToArray();
+    bool isSafe = true;
+    bool isDesc = levels[0] >= levels[1];
+
+    for (int i = 0; i < levels.Length - 1; i++)
+    {
+        int sign = isDesc ? 1 : -1;
+        int diff = sign * (levels[i] - levels[i + 1]);
+
+        if (diff < 1 || diff > 3)
+        {
+            isSafe = false;
+            break;
+        }
+    }
+    Console.WriteLine($"Is safe: {isSafe}");
+    safeReportCount += isSafe ? 1 : 0;
+}
+
+Console.WriteLine($"Result: {safeReportCount}");
